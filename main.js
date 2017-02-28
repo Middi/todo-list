b546f4414d9a68124cd5a3825bdb9407f816acf4
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-28 15:54:10 +0100

    Comments

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 var todos = ['item1', 'item2', 'item3'];
 
+
+// it should have a function to display todos.
 function displayTodos() {
 	console.log('My todos:', todos);
 }
 
-
+// it should have a function to add todos.
 function addTodo(todo) {
 	todos.push(todo);
 	displayTodos();
 }
 
+// it should have a function to change todos.
 function changeTodo(id, newValue) {
 	todos[id]  = newValue;
 	displayTodos();
 }
 
+// it should have a function to delete todos.
 function deleteTodo(id) {
 	todos.splice(id, 1);
 	displayTodos();
