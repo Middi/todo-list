9334babfc6cec694bc50ee834df248c5f1d336ef
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-28 16:35:29 +0100

    Created into object

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-var todos = ['item1', 'item2', 'item3'];
-
-
-// it should have a function to display todos.
-function displayTodos() {
-	console.log('My todos:', todos);
-}
-
-// it should have a function to add todos.
-function addTodo(todo) {
-	todos.push(todo);
-	displayTodos();
-}
-
-// it should have a function to change todos.
-function changeTodo(id, newValue) {
-	todos[id]  = newValue;
-	displayTodos();
-}
-
-// it should have a function to delete todos.
-function deleteTodo(id) {
-	todos.splice(id, 1);
-	displayTodos();
-}
+var todoList = {
+	todos: ['item1', 'item2', 'item3'],
+	displayTodos: function() {
+		console.log('My todos:', this.todos);
+	},
+	addTodo: function(todo) {
+		this.todos.push(todo);
+		this.displayTodos();
+	},
+	changeTodo: function(id, newValue) {
+		this.todos[id] = newValue;
+		this.displayTodos();
+	},
+	deleteTodo: function(id) {
+		this.todos.splice(id, 1);
+		this.displayTodos();
+	}
+};
