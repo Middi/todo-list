3b4e179477981d05fd2f993293daf5ea33f21fed
“Richard Middleton” <Richard@richardmiddleton.me>
2017-03-02 16:10:23 +0100

    Created completed if/else

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ var todoList = {
 	displayTodos: function() {
 		if (this.todos.length === 0) {
 			console.log('List empty');
-		} else {
+		} 
+		
+		else {
 			console.log('My todos:');
 			for (var i = 0; i < this.todos.length; i++) {
-				console.log(this.todos[i].todoText);
+
+				if (this.todos[i].completed === true) {
+					console.log('(x)', this.todos[i].todoText);
+				} 
+				else {
+					console.log('( )', this.todos[i].todoText);
+				}
 			}
 		}
 	},
