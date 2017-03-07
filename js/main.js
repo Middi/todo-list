1178b26605f0b15f17da1f5f6d8d56e771538356
“Richard Middleton” <Richard@richardmiddleton.me>
2017-03-07 14:21:12 +0100

    Added onclick handlers

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ var todoList = {
 	todos: [],
 
 	// Display list of Todos
-	displayTodos: function() {
+	displayTodos: function () {
+
 		if (this.todos.length === 0) {
 			console.log('List empty');
 		} else {
@@ -19,7 +20,7 @@ var todoList = {
 	},
 
 	// Add a todo to list
-	addTodo: function(todoText) {
+	addTodo: function (todoText) {
 		this.todos.push({
 			todoText: todoText,
 			completed: false
@@ -28,27 +29,26 @@ var todoList = {
 	},
 
 	// Edit a todo
-	changeTodo: function(id, todoText) {
+	changeTodo: function (id, todoText) {
 		this.todos[id].todoText = todoText;
 		this.displayTodos();
 	},
 
 	// Delete a todo
-	deleteTodo: function(id) {
+	deleteTodo: function (id) {
 		this.todos.splice(id, 1);
 		this.displayTodos();
 	},
 
 	// Toggle completed todo status
-	toggleCompleted: function(id) {
+	toggleCompleted: function (id) {
 		var todo = this.todos[id];
 		todo.completed = !todo.completed;
 		this.displayTodos();
 	},
 
 	// toggle all todos completed status
-	toggleAll: function() {
-
+	toggleAll: function () {
 		var totalTodos = this.todos.length;
 		var completedTodos = 0;
 
@@ -74,14 +74,11 @@ var todoList = {
 };
 
 // Get DOM elements
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function() {
-			todoList.displayTodos();
-});
-
-			toggleAllButton.addEventListener('click', function() {
-				todoList.toggleAll();
-
-			});
+var handlers = {
+	displayTodos: function () {
+		todoList.displayTodos();
+	},
+	toggleAll: function () {
+		todoList.toggleAll();
+	},
+};
