077bc37d986f0a219e794d901b19e9da7786b175
“Richard Middleton” <Richard@richardmiddleton.me>
2017-03-07 11:38:06 +0100

    Created multi select for completed tasks

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var todoList = {
 	todos: [],
-	displayTodos: function() {
+	displayTodos: function () {
 		if (this.todos.length === 0) {
 			console.log('List empty');
 		} 
@@ -12,31 +12,67 @@ var todoList = {
 				if (this.todos[i].completed === true) {
 					console.log('(x)', this.todos[i].todoText);
 				} 
+				
 				else {
 					console.log('( )', this.todos[i].todoText);
 				}
 			}
 		}
 	},
-	addTodo: function(todoText) {
+	
+	// Add a todo to list
+	addTodo: function (todoText) {
 		this.todos.push({
 			todoText: todoText,
 			completed: false
 		});
 		this.displayTodos();
 	},
-	changeTodo: function(id, todoText) {
+
+	// Edit a todo
+	changeTodo: function (id, todoText) {
 		this.todos[id].todoText = todoText;
 		this.displayTodos();
 	},
-	deleteTodo: function(id) {
+
+	// Delete a todo
+	deleteTodo: function (id) {
 		this.todos.splice(id, 1);
 		this.displayTodos();
 	},
-	toggleCompleted: function(id) {
+
+	// Toggle completed todo status
+	toggleCompleted: function (id) {
 		var todo = this.todos[id];
 		todo.completed = !todo.completed;
 		this.displayTodos();
+	},
 
+	// toggle all todos completed status
+	toggleAll: function () {
+
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+
+		// get number of completed todos
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++;
+			}
+		}
+
+		// if everything is true , make everything false.
+		if (completedTodos === totalTodos) {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+		} 
+		
+		else {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
+		this.displayTodos();
 	}
 };
